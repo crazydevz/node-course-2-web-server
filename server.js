0e4dd3fd67d9f56b78d1b54256396fb6f0cd5533
Talha Iqbal <talhaiqbal801@gamil.com>
2019-06-14 16:17:50 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         maintenanceHeading: 'We will be back',
+//         maintenanceMsg: 'The website is currently being updated'
+//     });
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+
+    fs.appendFile('server.log', log + "\n", (err) => {
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+
+    next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+// Handler for get request
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Welcome Page',
+        welcomeMessage: 'Welcome to the homepage'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    }); 
+})
+
+app.listen(3000, () => {
+    console.log('Server is up');
+});
